39f591a286a6539beb1ee73335668187b68821d0
mikimoy <rismaalis420@gmail.com>
2024-03-08 18:46:51 +0700

    first commit

---
+++ app/src/main/java/com/holycan/myapplication/MainActivity.java
@@ -0,0 +1,38 @@
+package com.holycan.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSignin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        btnSignin = (Button) findViewById(R.id.btnSignin);
+        
+        btnSignin.setOnClickListener(this::onCLick);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void onCLick(View view) {
+        startActivity(new Intent(MainActivity.this, MainActivity2.class));
+    }
+}
