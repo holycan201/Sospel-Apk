83174f717c2f3bfb4b1b09108e64c2f6dab8edd3
zeenkeun <153096814+zeenkeun@users.noreply.github.com>
2024-03-08 19:44:52 +0700

    add view for show input user name

--- app/src/main/java/com/holycan/myapplication/MainActivity2.java
+++ app/src/main/java/com/holycan/myapplication/MainActivity2.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity2 extends AppCompatActivity {
 
     Button signOut;
+    TextView tvUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,16 @@ public class MainActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
 
         signOut = (Button) findViewById(R.id.btnSignout);
+
+        tvUsername=(TextView) findViewById(R.id.tvUsername);
+        Intent myIntent= getIntent();
+        Bundle b = myIntent.getExtras();
+
+        if(b!=null)
+        {
+            String j =(String) b.get("usernm");
+            tvUsername.setText(j);
+        }
 
         signOut.setOnClickListener(this::onCLick);
 
